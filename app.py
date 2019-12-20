9615e5a91a19656d780e49ce83a96be7b0280003
Sam B <samuelbrus@gmail.com>
2019-12-20 16:41:40 -0600

    commented

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def send_message(msg):
     url  = 'https://api.groupme.com/v3/bots/post'
   
     #f841583c50c73aa774df650ffd  	LHH Chat
-    #44730fb274b2b3538d071f6f84		Tester
+    #44730fb274b2b3538d071f6f84		Tester NOT CONFIGURED YET
     data = {
         'bot_id' : 'f841583c50c73aa774df650ffd', #replace this code with above ones for corresponding GM
         'text'   : msg,
